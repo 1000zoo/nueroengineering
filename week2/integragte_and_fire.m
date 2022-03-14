230618073d77ca5f3a36f59a308c9e4687676dc1
1000zoo <cjswldn99@gmail.com>
2022-03-14 13:45:53 +0900

    week2

---
+++ week2/integragte_and_fire.m
@@ -0,0 +1,29 @@
+clear; clf;
+
+dt=0.1;
+tau=10;
+E_L=-65;
+theta=-55;
+RI_ext=24;
+
+t_step=0; v=E_L;
+for t=0:dt:100;
+    t_step=t_step+1;
+    s=v>theta;
+    v=s*E_L+(1-s)*(v-dt/tau*((v-E_L)-RI_ext));
+    v_rec(t_step)=v;
+    t_rec(t_step)=t;
+    s_rec(t_step)=s;
+end
+
+subplot('Position',[0.13 0.13 1-0.26 0.6])
+plot(t_rec, v_rec);
+hold on; plot([0 100], [-55 -55], '--');
+axis([0 100 -66 -54]);
+xlabel('Time [ms]'); ylabel('v [mV]');
+
+subplot('position', [0.13 0.8 1-0.26 0.1])
+plot(t_rec, s_rec, '.', 'markersize', 20);
+axis([0 100 0.5 1.5]);
+set(gca, 'xtick', [], 'ytick', [])
+ylabel('Spikes')
